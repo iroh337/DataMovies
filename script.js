2bbe5f1b4069c909d3b77ce97935759a23516e13
Richard Henrique <richardhs826@gmail.com>
2024-03-01 10:54:03 -0300

    DataMovies
    Changes in JS

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const search = document.querySelector("#search")
 
 search.addEventListener('click', getDataMovies)
 
-async function pushjson(){
+
+
+async function pushJson(){
     // const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKeyTmdb}&query=${movie}`).then(response => response.json())
     const pop_data = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKeyTmdb}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`).then(response=> response.json())
     init(pop_data)
@@ -20,16 +22,19 @@ async function pushjson(){
 
 function init(pop_data) {
     const other_movies = document.querySelector(".other-movies")
+    other_movies.innerHTML=''
     for( var i = 0 ; i < 12 ; i++){
-        var img = document.createElement('img')
-        img.src= `https://image.tmdb.org/t/p/w300/${pop_data.results[i].poster_path}`
-        img.className = ".img"
-        img.href = "youtube.com"
-        other_movies.appendChild(img)
+        
+        other_movies.innerHTML += `
+        <div class="movies movie-${i}" style="position:relative;">
+            <img src="https://image.tmdb.org/t/p/w300/${pop_data.results[i].poster_path}">
+            <a style="position:absolute;" href="./details/details.html"></a>
+        </div>
+        `
     }
     return init;
 }
-pushjson()
+pushJson()
 
 function clearAll(){
     const result = document.querySelector('.result')
@@ -45,11 +50,12 @@ async function getDataMovies(){
     console.log(data)
     
     other_movies.style.display = 'none'
-    for( var i = 0 ; i < 12 ; i++){
-        var img = document.createElement('img')
-        img.src= `https://image.tmdb.org/t/p/w300/${data.results[i].poster_path}`
-        img.innerHTML = `${data.results[i].original_title}`
-        img.className = ".img"
-        result.appendChild(img)
+    for( var i = 0 ; i < data.results.length-3 ; i++){
+        result.innerHTML += `
+        <div class="movies movie-${i}" style="position:relative;">
+            <img src="https://image.tmdb.org/t/p/w300/${data.results[i].poster_path}">
+            <a style="position:absolute;" href="https://www.youtube.com/?bp=wgUCEAE%3D"></a>
+        </div>
+        `
     }
 }
